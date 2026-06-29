e1d93eeb881fdd5d5ed7f1f6b993eefeb2f47225
mikail <engineer>
2026-06-29 13:23:03 +0100

    Fix CORS

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const bcrypt = require("bcrypt");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 let db;
 
